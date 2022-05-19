a47d39b20a2dcdd80f1a734512345f2d68160da9
Seb <seb@neonjungle.studio>
2022-05-19 11:45:05 +1000

    forgot icon ids

--- clean_icons.py
+++ clean_icons.py
@@ -5,11 +5,12 @@ from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
     # Convenience script for removing bad attributes and making the icons wagtail-able
-    icons_dir = Path(__file__).parent.joinpath('icomoon', 'templates', 'icons')
+    icons_dir = Path(__file__).parent.joinpath('wagtail_icomoon', 'templates', 'icons')
     for icon_file in icons_dir.glob('*.svg'):
         markup = BeautifulSoup(icon_file.read_text(), 'html.parser')
         for attr in ['xmlns', 'width', 'height']:
             for element in markup.find_all(attrs={attr: True}):
                 del element[attr]
-        icon_file.write_text(str(markup))
+        markup.find('svg').attrs['id'] = f'icon-{icon_file.stem}'
+        icon_file.write_text(markup.prettify())
         print(f'Cleaned {icon_file}')
