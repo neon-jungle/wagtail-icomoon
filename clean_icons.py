15662dc9e1f797daefd927d86a3bb485a4dcee0e
Seb Brown <seb@neonjungle.studio>
2025-05-12 14:48:15 +1000

    Lowercase the icons, per wagtail docs

--- clean_icons.py
+++ clean_icons.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 from pathlib import Path
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
     # Convenience script for removing bad attributes and making the icons wagtail-able
-    icons_dir = Path(__file__).parent.joinpath('wagtail_icomoon', 'templates', 'icons')
-    for icon_file in icons_dir.glob('*.svg'):
-        markup = BeautifulSoup(icon_file.read_text(), 'html.parser')
-        for attr in ['xmlns', 'width', 'height']:
+    icons_dir = Path(__file__).parent.joinpath("wagtail_icomoon", "templates", "icons")
+    for icon_file in icons_dir.glob("*.svg"):
+        markup = BeautifulSoup(icon_file.read_text(), "html.parser")
+        for attr in ["xmlns", "width", "height"]:
             for element in markup.find_all(attrs={attr: True}):
                 del element[attr]
-        markup.find('svg').attrs['id'] = f'icon-im-{icon_file.stem}'
+        markup.find("svg").attrs["id"] = f"icon-im-{icon_file.stem}".lower()
         icon_file.write_text(markup.prettify())
-        print(f'Cleaned {icon_file}')
+        print(f"Cleaned {icon_file}")
